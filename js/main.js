f9dad15df58cc745ac67dd2b375444a40924bdd8
Taras Trach <gamer2018cool@gamil.com>
2024-04-16 20:06:14 +0300

    Add Tabulation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
+const header = document.querySelector('.header');
+const startButton = document.querySelector('#startButton');
+const textArea = document.querySelector('#textarea-main');
+const textOverlay = document.querySelector('#textarea-overlay');
+
+
+startButton.addEventListener('click', startTest);
+
+const textArray = [
+   "async function fetchData() {",
+   "    try {",
+   "        const response = await fetch('https://example.com/data');",
+   "        const data = await response.json();",
+   "        console.log(data);",
+   "    } catch (error) {",
+   "        console.error('Error fetching data:', error);",
+   "    }",
+   "}"
+];
+
+
+
 function startTest() {
-   alert('start');
+   textArea.value = '';
+   header.innerHTML = 'Start typing..';
 
+   textArea.style.width = '700px';
+   textArea.style.height = '200px';
+
+   textOverlay.innerHTML = textArray.join('<br />');
+
+   textArea.addEventListener('keydown', function (e) { // Табуляція
+      if (e.key === 'Tab') {
+         e.preventDefault();
+
+         let start = this.selectionStart;
+         let end = this.selectionEnd;
+         let value = this.value;
+
+         this.value = value.substring(0, start) + '    ' + value.substring(end);
+         this.selectionStart = this.selectionEnd = start + 4;
+      }
+   });
+
+   textArea.addEventListener('input', function (e) {
+   });
 }
-
-let start = document.querySelector('#startButton');
-start.addEventListener('click', startTest);
